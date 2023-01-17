a2caeb380e32b689c50622115421e1cc647dc13d
BoitumeloL <brethabile25@gmail.com>
2023-01-17 12:22:49 +0200

    initial commit with web-cam face detection

---
+++ web_cam_detect_face.py
@@ -0,0 +1,68 @@
+import numpy as np
+import cv2 as cv
+
+#webcam detect code
+import sys
+import logging as log
+import datetime as dt
+from time import sleep
+
+#load classifiers
+face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv.CascadeClassifier('haarcascade_eye.xml')
+#webcam
+log.basicConfig(filename='webcam.log',level=log.INFO)
+
+#load image
+#img = cv.imread('sachin.jpg')
+#gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+video_capture = cv.VideoCapture(0)
+anterior = 0
+
+while True:
+    if not video_capture.isOpened():
+        print('Unable to load camera.')
+        sleep(5)
+        pass
+
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+    )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes:
+            cv.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(255,0,0),2)
+
+
+    if anterior != len(faces):
+        anterior = len(faces)
+        log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
+
+    
+    # Display the resulting frame
+    cv.imshow('Video', frame)
+
+
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    # Display the resulting frame
+    cv.imshow('Video', frame)
+
+# When everything is done, release the capture
+video_capture.release()
+cv.destroyAllWindows()
